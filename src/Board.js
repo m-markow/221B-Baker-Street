d1dcd79bf463b1ec375f2a4cb75de2ae421ebba0
m-markow <polmm132@gmail.com>
2022-05-11 21:56:46 +0200

    scraped map, implementation of Board class

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,16 @@
 var Board = /** @class */ (function () {
     function Board() {
         this.playersNumber = 0;
+        this.init();
     }
+    Board.prototype.init = function () {
+        var CANVAS = document.querySelector('#canva');
+        var ctx = CANVAS.getContext("2d");
+        var img = document.querySelector('#img');
+        img.onload = function () {
+            ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(img, -1400, -100);
+        };
+    };
     return Board;
 }());
 export default Board;
--- src/Board.ts
+++ src/Board.ts
@@ -4,5 +4,16 @@ export default class Board
 
     constructor() {
         this.playersNumber = 0;
+        this.init();
+    }
+
+    init()
+    {
+        const CANVAS: HTMLCanvasElement | null = document.querySelector('#canva');
+        let ctx = CANVAS!.getContext("2d");
+        let img: any = document.querySelector('#img');
+        img.onload = ()=> {
+            ctx?.drawImage(img, -1400, -100);
+        }
     }
 }
